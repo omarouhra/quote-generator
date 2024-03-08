83aa56713e9d1fd101647625780f71ab0210637b
Omar Ouhra <omarmaroc26@gmail.com>
2024-03-08 16:18:53 +0100

    feat: solution

--- src/App.js
+++ src/App.js
@@ -1,5 +1,33 @@
+import { useState } from "react";
+
 function App() {
-  return <div>Amigoscode React App Template</div>;
+  const [quote, setQuote] = useState();
+
+  const apiUrl = "https://api.quotable.io/random";
+
+  const getQuote = async () => {
+    try {
+      const response = await fetch(apiUrl);
+      const quote = await response.json();
+      setQuote(quote);
+    } catch (error) {
+      console.log("error", error);
+    }
+  };
+
+  return (
+    <div>
+      <div>
+        <button onClick={getQuote}>Get a New Quote</button>
+      </div>
+      {quote && (
+        <div>
+          <h1>"{quote.content}"</h1>
+          <p>{quote.author}</p>
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default App;
